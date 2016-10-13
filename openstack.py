a8f25449bdf290d45b5fa5cfc8aa33b1b34d69f3
chetu007 <chethankumara.s07@wipro.com>
2016-10-13 17:05:12 +0530

    Update openstack.py

--- openstack.py
+++ openstack.py
@@ -1,1 +1,12 @@
+# How to work with openstack
+#
+#It needs multiple options as: 
+# nova: to create instance
+# glance: to provide an OS for the vm
+# cinder: to provide a storage to the vm
+# keystone: to check the user credentials
+# neutron: to establish a network
 
+# all is needed is only a url, data, headers
+# with that generate a response by requesting a post with the url, data and headers
+# with that response generate a adminToken by response.json()
