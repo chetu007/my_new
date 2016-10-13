0cf63879729343cb63011f8ccde77585bca100fc
chetu007 <chethankumara.s07@wipro.com>
2016-10-13 17:33:00 +0530

    Update openstack.py

--- openstack.py
+++ openstack.py
@@ -10,3 +10,14 @@
 # all is needed is only a url, data, headers
 # with that generate a response by requesting a post with the url, data and headers
 # with that response generate a adminToken by response.json()
+
+# Procedure to create a user vm
+# First need to check with the credentials:
+
+# Create a network to which the vm to be connected
+
+# Assign an IP stream to the created network
+
+# Create a volume, size of the volumes is indicated by the numerals; for ex: 1-tiny, 2-small, 3-large etc
+
+# Create a instance and assign it a volume, connect to a network, assign an IP, provide an OS.
